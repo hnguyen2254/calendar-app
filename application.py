aa2d605ab596d62063246d105d38badad0aca37c
Warren Wang <wangwarren430@gmail.com>
2020-02-16 15:42:24 -0600

    flask

--- application.py
+++ application.py
@@ -1,14 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('login.html')
+@app.route('/', methods=["GET", "POST"])
+def login():
+	if request.method == "POST":
+		username = request.form.get("username")
+		print("TEST")
+		return redirect("/add")
+	else:
+		return render_template("login.html")
 
-@app.route('/event.html')
-def event():
-    return render_template('event.html')
+@app.route('/add', methods=["POST"])
+def add():
+    return render_template("add.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
