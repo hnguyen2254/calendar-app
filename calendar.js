9ae58c4af68fd4f7f1d90ff44ae8a96585feb64f
Jack Lunceford <jacklunceford@ku.edu>
2020-02-16 10:35:01 -0600

    Commit #2

--- calendar.js
+++ calendar.js
@@ -1,87 +1,29 @@
-let today = new Date();
-let currentMonth = today.getMonth();
-let currentYear = today.getFullYear();
-let selectYear = document.getElementById("year");
-let selectMonth = document.getElementById("month");
+document.addEventListener("DOMContentLoaded", function(){
+    var startTime = moment().utc().set({hour:00,minute:00});
+var endTime = moment().utc().set({hour:11,minute:59});
 
-let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+ var timeStops = [];
 
-let monthAndYear = document.getElementById("monthAndYear");
-showCalendar(currentMonth, currentYear);
-
-
-function next() {
-    currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
-    currentMonth = (currentMonth + 1) % 12;
-    showCalendar(currentMonth, currentYear);
+while(startTime <= endTime){
+    timeStops.push(new moment(startTime).format('HH:mm'));
+    startTime.add(15, 'minutes');
+}
+for (const val of a) {
+    var opt = document.createElement("option");
+    opt.textContent = val;
+    opt.value = val;
+    select.appendChild(opt);
 }
 
-function previous() {
-    currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
-    currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
-    showCalendar(currentMonth, currentYear);
-}
-
-function jump() {
-    currentYear = parseInt(selectYear.value);
-    currentMonth = parseInt(selectMonth.value);
-    showCalendar(currentMonth, currentYear);
-}
-
-function showCalendar(month, year) {
-
-    let firstDay = (new Date(year, month)).getDay();
-    let daysInMonth = 32 - new Date(year, month, 32).getDate();
-
-    let tbl = document.getElementById("calendar-body"); // body of the calendar
-
-    // clearing all previous cells
-    tbl.innerHTML = "";
-
-    // filing data about month and in the page via DOM.
-    monthAndYear.innerHTML = months[month] + " " + year;
-    selectYear.value = year;
-    selectMonth.value = month;
-
-    // creating all cells
-    let date = 1;
-    for (let i = 0; i < 6; i++) {
-        // creates a table row
-        let row = document.createElement("tr");
-
-        //creating individual cells, filing them up with data.
-        for (let j = 0; j < 7; j++) {
-            if (i === 0 && j < firstDay) {
-                let cell = document.createElement("td");
-                var btn = document.createElement('input');
-                btn.type = "button";
-                btn.className = "btn";
-                btn.value = "";
-                cell.appendChild(btn);
-                row.appendChild(cell);
-            }
-            else if (date > daysInMonth) {
-                break;
-            }
-
-            else {
-                let cell = document.createElement("td");
-                var btn = document.createElement('input');
-                btn.type = "button";
-                btn.className = "btn";
-                btn.value = date;
-                cell.appendChild(btn);
-                if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
-                    cell.classList.add("bg-info");
-                } // color today's date
-                row.appendChild(cell);
-                date++;
-            }
-
-
-        }
-
-        tbl.appendChild(row); // appending each row into calendar body.
-    }
-
-}
+    Date.prototype.toDateInputValue = (function(){
+        var local = new Date(this);
+        local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+        return local.toJSON().slice(0,10);
+    });
+   // if (document.getElementById("event").checked) {
+      //  window.location = "event.html";
+  //}
+    document.getElementById("datePicker").value = new Date().toDateInputValue();
+    document.getElementById("datePicker").min = new Date().toDateInputValue();
+    var select = document.getElementById("24"); 
+});
